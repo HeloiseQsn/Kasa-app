012dd5aff4f65969b037f0bf1b00b9f7990849a6
Héloïse <heloise.lafontaine@gmail.com>
2024-11-07 15:35:41 +0100

    Mise en forme tag

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,7 +1,11 @@
 import './Tag.scss'
 
 function Tag({ tag }) {
-  return <div className="tag">{tag}</div>
+  return (
+    <div className="tag">
+      <div className="tag_text">{tag}</div>
+    </div>
+  )
 }
 
 export default Tag
