06a77e77a345a8a951a9e1e4aacd2a9372a27ed8
Héloïse <heloise.lafontaine@gmail.com>
2024-11-16 18:25:37 +0100

    ajout variables, maj mixin police

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,6 +4,7 @@ import './Card.scss'
 
 function Card({ cardData }) {
   return (
+    //Lien vers logement : on construit le chemin url avec l'ID de l'hébergement que l'on récupère dans la cardData
     <Link to={`/accommodation/${cardData.id}`} className="card">
       <article className="card__article">
         <img src={cardData.cover} alt={cardData.title} />
